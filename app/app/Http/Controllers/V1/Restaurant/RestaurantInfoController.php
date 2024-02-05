ac9c1fd7022bc94d3a2a3f0bb9a2e7e68ce534cd
Rifat <rifatcse09@gmail.com>
2024-02-05 21:18:33 +0600

    controller update

--- app/app/Http/Controllers/V1/Restaurant/RestaurantInfoController.php
+++ app/app/Http/Controllers/V1/Restaurant/RestaurantInfoController.php
@@ -7,7 +7,6 @@ namespace App\Http\Controllers\V1\Restaurant;
 use App\Models\RestaurantInfo;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-// use Illuminate\Support\Facades\Validator;
 use App\Services\V1\Restaurant\RestaurantService;
 use App\Http\Requests\V1\Restaurant\RestaurantCreateRequest;
 
@@ -19,26 +18,15 @@ final class RestaurantInfoController extends Controller
 
     public function store(RestaurantCreateRequest $request) : JsonResponse
     {
-        // $validator = Validator::make($request->all(), [
-        //     'name' => 'required',
-        //     'lat'  => 'required',
-        //     'long' => 'required',
-        // ]);
-
-        // if ($validator->fails()) {
-        //     //return response()->json(['status' => 'error', 'errors' => $validator->messages()], Response::HTTP_UNPROCESSABLE_ENTITY);
-        //     return api(['errors' => $validator->messages()])->fails();
-        // }
-
         $data = $this->restaurantService->createRestaurant($request->all());
 
         return api([$data])->success('Restaurants Created Successfully');
     }
 
-    public function getRestaurant($id) : JsonResponse
+    public function getRestaurant(int $id) : JsonResponse
     {
         $data = RestaurantInfo::find($id);
 
-        return api([$data])->success('Data found');
+        return api([$data])->success('Restaurants Created Successfully');
     }
 }
